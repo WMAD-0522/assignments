8430b2bdb1562fefa805ddb38adb51d1249e8e72
Hiroki <h.m.jojo.8258@gmail.com>
2023-01-18 02:51:04 -0800

    almost finish

--- todoApp/routes/todo.js
+++ todoApp/routes/todo.js
@@ -66,9 +66,10 @@ router.get("/edit/:id", (req, res) => {
         // find the index
         let todos = JSON.parse(data);
         let todo = todos.find(todo => todo.id == req.params.id);
-        let index = todos.indexOf(todo);
+
         // send to the render of edit page,
         res.render('pages/edit', {
+            title: 'Edit todo',
             todo: todo
         });
     })
@@ -82,20 +83,11 @@ router.post("/edit/:id", (req, res)=> {
     // find index
     let todos = JSON.parse(data);
     let todo = todos.find(todo => todo.id == req.params.id);
-    let index = todos.indexOf(todo);
+    
     todo.title = req.body.title;
     todo.description = req.body.description;
     todo.completed = req.body.completed;
-    //change object related with request body
-    // todos.splice(index, 1);
-    // let editedTodo = {
-    //     id: todo.id,
-    //     title: req.body.title,
-    //     description: req.body.description,
-    //     completed: req.body.completed
-    // }
-    // todos.splice(index, 0, editedTodo);
-    // write file again
+
     fs.write('./public/data/todos.json', JSON.stringify(todos), (err) => {
         if(err) throw err;
         // redirect
